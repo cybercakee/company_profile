69899be600d41890883703115c954b973888f108
restusatyaw <kadekrestusatyawardana@gmail.com>
2024-11-20 10:08:17 +0800

    config page title

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Kaiadmin - Bootstrap 5 Admin Dashboard</title>
+    <title>{{config('app.name')}} | {{$page_title}}</title>
     <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
     <link rel="icon" href="{{asset('admin_assets/img/kaiadmin/favicon.ico')}}" type="image/x-icon" />
     @stack('style')
